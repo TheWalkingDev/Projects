e1463f9a078070e4f48566a27c07e9941f21101e
Andre <Adrenaline@.(none)>
2011-05-30 17:56:40 +0200

    commit

---
+++ DevTools/PortListener/PortListener/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace PortListener
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            new Program().Run(args);
+           
+        }
+
+        private void Run(string[] args)
+        {
+            int port = Int32.Parse(args[0]);
+            Console.WriteLine("Started listening on port: {0}", port);
+            var server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+            server.BeginAcceptSocket(new AsyncCallback(server_onAccept), server);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        void server_onAccept(IAsyncResult ar)
+        {
+            var server = ((TcpListener)ar.AsyncState);
+            var socket = server.EndAcceptSocket(ar);
+            
+            Console.WriteLine(string.Format("Socket {0} Connected",socket.RemoteEndPoint.ToString()));
+            server.BeginAcceptSocket(new AsyncCallback(server_onAccept), server);
+        }
+    }
+}
--- DevTools/PyPad/PyPad/Program.cs
+++ DevTools/PyPad/PyPad/Program.cs
@@ -13,9 +13,18 @@ namespace PyPad
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                Application.Exit();
+                Application.DoEvents();
+            }
         }
     }
 }
